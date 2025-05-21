c3e859aa8e670f107304ac59a23b6d63ada1d0aa
cvvz <ftdchenwz@gmail.com>
2025-05-21 15:14:05 +0800

    rm script

--- workspace/tuning/dataset_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-IMAGE_NAME="kaitosample.azurecr.io/go-operator-reviewer-data:0.0.1"
-
-az acr login --name kaitosample && \
-docker build -t $IMAGE_NAME . && \
-docker push $IMAGE_NAME
